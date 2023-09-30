2e004b1b029ee60c85f63f3273ca375890ebff3a
황수연 <202918@jnu.ac.kr>
2023-09-30 19:40:02 +0900

    rename: 로그인/회원가입 스타일 하나로 합치기

---
+++ src/styles/organisms/UserInputForm.ts
@@ -0,0 +1,59 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 90vh;
+  width: 100vw;
+  @media only screen and (max-width: 768px) {
+    overflow: hidden;
+  }
+
+  .welcome__text {
+    margin: 1rem;
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 1.2em;
+  margin-bottom: 0.8em;
+  font-weight: normal;
+`;
+
+export const Box = styled.div`
+  border: 1px solid #d8d8d8;
+  padding: 2rem;
+  @media only screen and (max-width: 768px) {
+    border-width: 0;
+    padding-right: 0;
+    padding-left: 0;
+  }
+`;
+
+export const Button = styled.button`
+  background-color: white;
+  border: ${(props) =>
+    props.disabled ? '1px solid #c8c8c8' : '1px solid #a59d52'};
+  border-radius: 20px;
+  width: 25.5rem;
+  height: 2.7rem;
+  margin-top: 1.5rem;
+  font-size: 1rem;
+  color: ${(props) => (props.disabled ? '#8c8c8c' : 'black')};
+  &:hover,
+  &:active {
+    background-color: #eba059;
+    color: white;
+    cursor: pointer;
+    border: none;
+  }
+
+  @media only screen and (max-width: 768px) {
+    width: 81.5vw;
+    margin-left: 10vw;
+    margin-right: 10vw;
+  }
+  pointer-events: ${(props) => (props.disabled ? 'none' : null)};
+`;
