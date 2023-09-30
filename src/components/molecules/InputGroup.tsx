7cec0771b184f97802ad00674a2bd7451d189c6b
황수연 <202918@jnu.ac.kr>
2023-09-30 16:19:51 +0900

    feat: 사용자 입력 관리

---
+++ src/components/molecules/InputGroup.tsx
@@ -0,0 +1,49 @@
+import Box from '../atoms/Box';
+import * as InputBox from '../../styles/molecules/InputGroup';
+import ErrorMsg from '../atoms/ErrorMsg';
+
+type InputProps = {
+  id: string;
+  name: string;
+  type: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  className: string;
+  label: string;
+  placeholder: string;
+  onBlur: Function;
+  invalid: { [key: string]: string };
+};
+
+import { FocusEventHandler } from 'react';
+
+const InputGroup = ({
+  id,
+  name,
+  type,
+  value,
+  onChange,
+  className,
+  label,
+  placeholder,
+  onBlur,
+  invalid,
+}: InputProps) => {
+  return (
+    <Box className={className}>
+      <InputBox.Label htmlFor={id}>{label}</InputBox.Label>
+      <InputBox.Input
+        id={id}
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        onBlur={onBlur as FocusEventHandler<HTMLInputElement>}
+      />
+      <ErrorMsg errorMsg={invalid} name={name} />
+    </Box>
+  );
+};
+
+export default InputGroup;
