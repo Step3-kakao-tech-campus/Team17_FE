ed9d612357beb81d1a7229d4318da393833901c8
황수연 <202918@jnu.ac.kr>
2023-09-30 19:38:07 +0900

    design: 로그인/회원가입 텍스트 스타일 변경

--- src/styles/atoms/Link.ts
+++ src/styles/atoms/Link.ts
@@ -1,22 +1,35 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-export const SignUp = styled.div`
-  font-size: 0.8em;
-  margin-top: 1.5em;
-  @media only screen and (max-width: 700px) {
+export const TextContainer = styled.div`
+  font-size: 0.9rem;
+  margin-top: 1.5rem;
+  display: flex;
+  justify-content: space-between;
+  @media only screen and (max-width: 768px) {
     margin-left: 10vw;
+    margin-right: 10vw;
   }
-`;
-export const Login = styled.div`
-  font-size: 0.8em;
-  margin-top: 1.5em;
-  @media only screen and (max-width: 700px) {
-    margin-left: 10vw;
+
+  .register__text,
+  .login__text {
+    color: inherit;
+  }
+
+  .login__check {
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+
+    .check__icon {
+      padding-right: 5px;
+    }
   }
 `;
 
 export const Text = styled(Link)`
   text-decoration: none;
-  color: black;
+  &:hover {
+    color: black;
+  }
 `;
