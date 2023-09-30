820f11faa983d3e2d3f4c9d9c7ae5b4d42598965
황수연 <202918@jnu.ac.kr>
2023-09-30 19:39:15 +0900

    design: 로그인/회원가입 기본 스타일 지정

--- src/styles/molecules/InputGroup.ts
+++ src/styles/molecules/InputGroup.ts
@@ -1,17 +1,19 @@
 import styled from 'styled-components';
 
 export const Input = styled.input`
-  border-radius: 4px;
+  border-radius: 7px;
   border-width: 1px;
-  border: 1px solid #d8d8d8;
-  width: 25em;
-  height: 2em;
-  font-size: 1em;
+  border: 1px solid #f1f1f1;
+  background-color: #f1f1f1;
+  width: 25rem;
+  height: 2rem;
+  font-size: 1rem;
   box-sizing: content-box;
-  margin-top: 0.5em;
-  margin-bottom: 0.5em;
+  margin-top: 0.5rem;
+  margin-bottom: 0.5rem;
   padding-left: 7px;
-  @media only screen and (max-width: 700px) {
+  color: black;
+  @media only screen and (max-width: 768px) {
     width: 80vw;
     margin-left: 10vw;
     margin-right: 10vw;
@@ -19,9 +21,9 @@ export const Input = styled.input`
 `;
 
 export const Label = styled.label`
-  font-size: 1em;
+  font-size: 1rem;
   display: block;
-  @media only screen and (max-width: 700px) {
+  @media only screen and (max-width: 768px) {
     margin-left: 10vw;
   }
 `;
