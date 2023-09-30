f5fdf7781b27168b857a0809ba54cfcc1ff95e03
황수연 <202918@jnu.ac.kr>
2023-09-30 21:23:26 +0900

    feat: welcome page 경로 추가
    figma 회원가입 및 로그인 기능의 welcome 페이지

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
+import WelcomePage from './pages/WelcomePage';
+import { Reset } from 'styled-reset';
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/signup" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </Router>
+    <>
+      <Reset />
+      <Router>
+        <Routes>
+          <Route path="/signup" element={<RegisterPage />} />
+          <Route path="/signin" element={<LoginPage />} />
+          <Route path="/welcome" element={<WelcomePage />} />
+        </Routes>
+      </Router>
+    </>
   );
 };
 
