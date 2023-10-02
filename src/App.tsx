9c90e60d714f113b2c461422761d1d8ee11dbe2b
황수연 <202918@jnu.ac.kr>
2023-10-02 17:57:03 +0900

    feat: onboard 경로 변경
    onboard 경로 변경 및 사용하지 않는 welcome 경로 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
-import WelcomePage from './pages/WelcomePage';
 import { Reset } from 'styled-reset';
-import OnBoarding from './pages/onboarding/OnBoarding';
+import OnBoarding from './components/organisms/onBoardingList';
 
 const App = () => {
   return (
@@ -11,8 +10,7 @@ const App = () => {
       <Reset />
       <Router>
         <Routes>
-          <Route path="/onBoard" element={<OnBoarding />} />
-          <Route path="/welcome" element={<WelcomePage />} />
+          <Route path="/onboard" element={<OnBoarding />} />
           <Route path="/signup" element={<RegisterPage />} />
           <Route path="/signin" element={<LoginPage />} />
         </Routes>
