eb27653f7866de1b9cf49756f0ebd9828ac05b33
YANGSEOKWOO <didtjrdn030@naver.com>
2023-10-05 21:58:38 +0900

    feat: list 게시물 컴포넌트 추가

--- src/components/atoms/List.tsx
+++ src/components/atoms/List.tsx
@@ -1,24 +1,82 @@
 import React from 'react';
-
+import styled from 'styled-components';
+import { CaretRight } from '@phosphor-icons/react';
+import Photo from './Photo';
 type ListProps = {
-  className: string;
-  breed: string;
-  age: number;
-  date: string;
-  title: string;
+  className?: string;
+  breed?: string;
+  age?: number;
+  date?: string;
+  title?: string;
 };
-
+{
+  /* <img src="./images/logo.png" alt="check" />
+<div>클래스 : {className}</div>
+<div>품종 : {breed}</div>
+<div>나이 : {age}</div>
+<div>날짜 : {date}</div>
+<div>내용 : {title}</div> */
+}
 const List = ({ className, breed, age, date, title }: ListProps) => {
   return (
-    <>
-      <img src="./images/logo.png" alt="check" />
-      <div>클래스 : {className}</div>
-      <div>품종 : {breed}</div>
-      <div>나이 : {age}</div>
-      <div>날짜 : {date}</div>
-      <div>내용 : {title}</div>
-    </>
+    <StyleContainer>
+      <PictureContainer>
+        <Photo src="./images/logo.png" alt="강아지사진"></Photo>
+        {/* <img  /> */}
+      </PictureContainer>
+      <StyleContent>
+        <StyleTitle>
+          <span>시바견 </span>
+          <span>3살 </span>
+          <span>23.12.25 15:00~ 16:00 </span>
+        </StyleTitle>
+        <span> 산책완료</span>
+      </StyleContent>
+      <ArrowContent>
+        <CaretRight color="white" size={30} />
+      </ArrowContent>
+    </StyleContainer>
   );
 };
 
 export default List;
+
+const StyleContainer = styled.div`
+  border-radius: 20px;
+  height: 80px;
+  display: flex;
+  background-color: #d6cfa5;
+  justify-content: space-between;
+`;
+
+const PictureContainer = styled.div`
+  flex: 1;
+  width: 60px;
+  height: 60px;
+  margin: 10px 10px 10px 20px;
+  /* margin: px; */
+  /* text-align: center; */
+`;
+
+const StyleContent = styled.div`
+  flex: 5;
+  margin: 10px 0 10px 0;
+  & > span {
+    display: block;
+    margin-top: 10px;
+    font-size: 24px;
+    color: #ffffff;
+  }
+`;
+
+const ArrowContent = styled.div`
+  margin: 25px 0 25px 0;
+  flex: 1;
+`;
+
+const StyleTitle = styled.div`
+  & > span {
+    font-weight: bold;
+  }
+`;
+const StyleMessage = styled.div``;
