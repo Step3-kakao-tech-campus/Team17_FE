80b8d4be0da9c09a03f645ceacce26196c0f7722
황수연 <202918@jnu.ac.kr>
2023-10-07 21:46:56 +0900

    feat: 메인 페이지 헤더

---
+++ src/components/organisms/MainGNB.tsx
@@ -0,0 +1,18 @@
+import Image from '../atoms/Image'
+import SearchBar from '../molecules/SearchBar';
+import * as S from '../../styles/organisms/MainGNB';
+
+
+const MainGNB = () => {
+  return (
+    <S.Container>
+      <Image src="/images/dog_logo.png" alt="dog logo" size="1.5"/>
+      <SearchBar/>
+      <S.ProfileWrapper>
+        <Image src="/images/test.png" alt="default profile"  className='profile__image'/>
+      </S.ProfileWrapper>
+    </S.Container>
+  )
+}
+
+export default MainGNB
