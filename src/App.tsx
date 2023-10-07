0831a01595c2d1d2c39126f61da2da441f45ccac
황수연 <202918@jnu.ac.kr>
2023-10-07 21:48:42 +0900

    feat: 메인 페이지 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import RegisterPage from './pages/RegisterPage';
 import LoginPage from './pages/LoginPage';
 import { Reset } from 'styled-reset';
 import OnBoardingPage from './pages/OnBoardingPage';
+import HomePage from './pages/MainPage';
+import MainLayout from './components/layouts/MainLayout';
 
 const App = () => {
   return (
@@ -10,9 +12,12 @@ const App = () => {
       <Reset />
       <Router>
         <Routes>
-          <Route path="/onboard" element={<OnBoardingPage />} />
-          <Route path="/signup" element={<RegisterPage />} />
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<HomePage />} />
+          </Route>
           <Route path="/signin" element={<LoginPage />} />
+          <Route path="/signup" element={<RegisterPage />} />
+          <Route path="/onboard" element={<OnBoardingPage />} />
         </Routes>
       </Router>
     </>
