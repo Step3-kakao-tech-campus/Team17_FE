cd4bdb0b6229b96a7c22deb664e2e04fce5535c9
황수연 <202918@jnu.ac.kr>
2023-10-07 21:51:21 +0900

    design: 하단바 스타일 지정

---
+++ src/styles/molecules/BottomNavBar.ts
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  border-top: 0.5px solid #d9d9d9;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 0.6rem 0;
+  z-index: 100;
+  background-color: white;
+  height: 4rem;
+
+  .active {
+    color: #a59d52;
+  }
+  box-sizing: border-box;
+`;
+
+export const NavItemWrapper = styled.span`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
+
+export const BottomNavText = styled.h1`
+  display: inline;
+  font-size: 0.8rem;
+  color: #999999;
+  padding-top: 0.2rem;
+`;
