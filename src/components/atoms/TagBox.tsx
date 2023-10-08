1c3025bc5b5c63a41aea8944605c6f29224d2b9f
황수연 <202918@jnu.ac.kr>
2023-10-08 19:06:40 +0900

    style: 동적 스타일 지정

--- src/components/atoms/TagBox.tsx
+++ src/components/atoms/TagBox.tsx
@@ -1,16 +1,41 @@
-import * as S from '../../styles/atoms/TagBox'
+import * as S from '../../styles/atoms/TagBox';
 
 type TagBoxProps = {
-  innerText: string,
-  color?: string,
-}
+  innerText: string;
+  borderColor?: string;
+  color?: string;
+  size?: string;
+  width?: string;
+  backColor?: string;
+  className?: string;
+  onClick?: () => void;
+};
 
-const TagBox = ({innerText, color}: TagBoxProps) => {
+const TagBox = ({
+  innerText,
+  borderColor,
+  color,
+  size,
+  width,
+  className,
+  onClick,
+  backColor,
+}: TagBoxProps) => {
   return (
-    <S.Tag style={{border: `1px solid ${color}`, color: color}}>
+    <S.Tag
+      style={{
+        border: `1px solid ${borderColor}`,
+        color: color,
+        fontSize: `${size}rem`,
+        fontWeight: width,
+        backgroundColor: backColor,
+      }}
+      className={className}
+      onClick={onClick}
+    >
       {innerText}
     </S.Tag>
-  )
-}
+  );
+};
 
-export default TagBox
+export default TagBox;
