855fe8e0605c7bb7c901332f8a9780a321466baa
황수연 <202918@jnu.ac.kr>
2023-10-08 19:10:30 +0900

    feat: 캐러셀 클릭 시 해당 페이지로 이동
    광고용 캐러셀 컴포넌트 클릭 시 해당 웹 사이트로 이동하도록 수정

--- src/components/molecules/Carousel.tsx
+++ src/components/molecules/Carousel.tsx
@@ -1,4 +1,4 @@
-import { Swiper, SwiperSlide } from 'swiper/react'
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Navigation, Autoplay } from 'swiper/modules';
 import * as S from '../../styles/molecules/Carousel';
 
@@ -12,28 +12,43 @@ const Carousel = () => {
       <Swiper
         modules={[Pagination, Navigation, Autoplay]}
         loop={true}
-				navigation
-				pagination={{ clickable: true }}
+        navigation
+        pagination={{ clickable: true }}
         autoplay={{ delay: 3000, disableOnInteraction: false }}
       >
         <SwiperSlide>
           <S.Wrap>
-            <img src="/images/main-carousel9.jpg" alt="캐러셀1" />
+            <a
+              href="https://www.knrc.co.kr/shop/list.php?dog_or_cat=%EA%B0%95%EC%95%84%EC%A7%80"
+              target="_blank"
+            >
+              <img src="/images/main-carousel9.jpg" alt="캐러셀1" />
+            </a>
           </S.Wrap>
         </SwiperSlide>
         <SwiperSlide>
           <S.Wrap>
-            <img src="/images/main-carousel10.jpg" alt="캐러셀1" />
+            <a
+              href="https://www.knrc.co.kr/shop/list.php?dog_or_cat=%EA%B0%95%EC%95%84%EC%A7%80"
+              target="_blank"
+            >
+              <img src="/images/main-carousel10.jpg" alt="캐러셀1" />
+            </a>
           </S.Wrap>
         </SwiperSlide>
         <SwiperSlide>
           <S.Wrap>
-            <img src="/images/main-carousel12.png" alt="캐러셀1" />
+            <a
+              href="https://m.wooof.co.kr/product/detail.html?product_no=16711&cate_no=0&display_group=1"
+              target="_blank"
+            >
+              <img src="/images/main-carousel12.png" alt="캐러셀1" />
+            </a>
           </S.Wrap>
         </SwiperSlide>
       </Swiper>
     </S.Container>
-  )
-}
+  );
+};
 
-export default Carousel
+export default Carousel;
