382ff21a6d3c66ea063ad110f0774f615a798f50
황수연 <202918@jnu.ac.kr>
2023-10-08 19:08:33 +0900

    feat: 각 네비게이션 아이콘 추가
    네비게이션 아이콘 및 크기, 색상 지정

--- src/components/molecules/BottomNavBar.tsx
+++ src/components/molecules/BottomNavBar.tsx
@@ -3,10 +3,12 @@ import * as S from '../../styles/molecules/BottomNavBar';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+// 하단 네비게이션 바
 const BottomNavBar = () => {
   const [active, setActive] = useState(0);
   const navigate = useNavigate();
 
+  // 클릭 시 해당 페이지로 이동
   const handleActive = (index: number) => {
     setActive(index);
 
@@ -23,24 +25,42 @@ const BottomNavBar = () => {
     //   default:
     //     break;
     // }
-  }
+  };
 
   return (
     <S.Container>
       <S.NavItemWrapper onClick={() => handleActive(0)}>
-        <House size={25} weight={active === 0 ? 'fill' : 'regular'} color={active === 0 ? '#A59D52' : '#999999'}/>
-        <S.BottomNavText className={active === 0 ? 'active' : ''}>home</S.BottomNavText>
+        <House
+          size={25}
+          weight={active === 0 ? 'fill' : 'regular'}
+          color={active === 0 ? '#A59D52' : '#999999'}
+        />
+        <S.BottomNavText className={active === 0 ? 'active' : ''}>
+          home
+        </S.BottomNavText>
       </S.NavItemWrapper>
       <S.NavItemWrapper onClick={() => handleActive(1)}>
-        <ChatsCircle size={25} weight={active === 1 ? 'fill' : 'regular'} color={active === 1 ? '#A59D52' : '#999999'}/>
-        <S.BottomNavText className={active === 1 ? 'active' : ''}>chat</S.BottomNavText>
+        <ChatsCircle
+          size={25}
+          weight={active === 1 ? 'fill' : 'regular'}
+          color={active === 1 ? '#A59D52' : '#999999'}
+        />
+        <S.BottomNavText className={active === 1 ? 'active' : ''}>
+          chat
+        </S.BottomNavText>
       </S.NavItemWrapper>
       <S.NavItemWrapper onClick={() => handleActive(2)}>
-        <User size={25} weight={active === 2 ? 'fill' : 'regular'} color={active === 2 ? '#A59D52' : '#999999'}/>
-        <S.BottomNavText className={active === 2 ? 'active' : ''}>profile</S.BottomNavText>
+        <User
+          size={25}
+          weight={active === 2 ? 'fill' : 'regular'}
+          color={active === 2 ? '#A59D52' : '#999999'}
+        />
+        <S.BottomNavText className={active === 2 ? 'active' : ''}>
+          profile
+        </S.BottomNavText>
       </S.NavItemWrapper>
     </S.Container>
-  )
-}
+  );
+};
 
-export default BottomNavBar
+export default BottomNavBar;
