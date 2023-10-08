6006bcccd4829396187f6ae27d4103a5f590289f
황수연 <202918@jnu.ac.kr>
2023-10-08 19:47:21 +0900

    chore: 스타일 초기화 모듈 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
-import { Reset } from 'styled-reset';
 import OnBoarding from './pages/OnBoarding';
 import Main from './pages/Main';
 import MainLayout from './components/layouts/MainLayout';
@@ -9,7 +8,6 @@ import MainLayout from './components/layouts/MainLayout';
 const App = () => {
   return (
     <>
-      <Reset />
       <Router>
         <Routes>
           <Route element={<MainLayout />}>
