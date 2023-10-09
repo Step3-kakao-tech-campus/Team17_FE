b17c4ddb45047de3805da481446edcfc8a0699ce
황수연 <202918@jnu.ac.kr>
2023-10-09 22:14:25 +0900

    feat: 사용자 로그인 상태 받아오는 코드 작성

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,15 +2,40 @@ import Location from '../components/molecules/Location';
 import Carousel from '../components/molecules/Carousel';
 import BottomNavBar from '../components/molecules/BottomNavBar';
 import MainListTemplate from '../components/templates/MainListTemplate';
+import * as S from '../styles/layout/MainLayout';
+import MainGNB from '../components/organisms/MainGNB';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { AppDispatch, RootState } from '../store';
+import { setUser } from '../store/slices/userSlice';
+import { getLocalStorage } from '../utils/localStorage';
 
 const Main = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  // 사용자가 메인 페이지에 접근했을 때 브라우저에서 사용자 로그인 상태 확인
+  // const dispatch = useDispatch<AppDispatch>();
+  // const user = useSelector((state: RootState) => state.user.user);
+  // useEffect(() => {
+  //   try {
+  //     const userStorage = getLocalStorage('user');
+  //     const isLogin = userStorage ? JSON.parse(userStorage) : null;
+  //     if (isLogin) {
+  //       // 로그인이 된 상태, 상태값 저장
+  //       dispatch(setUser({ user: isLogin.value }));
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }, [dispatch, user]);
+
   return (
-    <>
+    <S.Container>
+      <MainGNB setModalOpen={setModalOpen} />
       <Location />
       <Carousel />
-      <MainListTemplate />
+      <MainListTemplate modalOpen={modalOpen} setModalOpen={setModalOpen} />
       <BottomNavBar />
-    </>
+    </S.Container>
   );
 };
 
