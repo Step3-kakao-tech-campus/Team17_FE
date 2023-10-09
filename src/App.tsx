2adefb1986f1f3379f847a55f4e00c1ecfb7ef4d
황수연 <202918@jnu.ac.kr>
2023-10-09 22:03:36 +0900

    feat: react-query 적용
    - react-query 적용
    - mainlayout 파일 삭제로 인한 경로 수정

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,27 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import OnBoarding from './pages/OnBoarding';
 import Main from './pages/Main';
-import MainLayout from './components/layouts/MainLayout';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { SetStateAction } from 'react';
 
 const App = () => {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Router>
         <Routes>
-          <Route element={<MainLayout />}>
-            <Route path="/" element={<Main />} />
-          </Route>
           <Route path="/signin" element={<Login />} />
           <Route path="/signup" element={<Register />} />
           <Route path="/onboard" element={<OnBoarding />} />
+          {/* <Route element={<MainLayout />}> */}
+          <Route path="/" element={<Main />} />
+          {/* </Route> */}
         </Routes>
       </Router>
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
