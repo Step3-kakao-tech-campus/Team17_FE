0e198c2a5aa6472b8d1a1548ac5b8285d2466f99
황수연 <202918@jnu.ac.kr>
2023-10-11 22:12:41 +0900

    feat: 사용자 현재 위치 출력 로직 추가

--- src/components/molecules/Location.tsx
+++ src/components/molecules/Location.tsx
@@ -1,15 +1,49 @@
 import { MapPin, ArrowCounterClockwise } from '@phosphor-icons/react';
 import * as S from '../../styles/molecules/Location';
+import useGeolocation from '../../hooks/useGeolocation';
+import { useEffect, useState } from 'react';
+import kakaoLocation from '../../utils/kakaoLocation';
 
 const Location = () => {
+  const [locate, setLocate] = useState({
+    lat: 0,
+    lng: 0,
+  });
+  const [address, setAddress] = useState('');
+  const location = useGeolocation();
+
   const handleRefresh = () => {
-    // 새로고침 혹은 사용자 위치 재설정
+    // 새로고침하여 위치 재설정
     window.location.reload();
   };
 
+  useEffect(() => {
+    if (location.coordinates) {
+      setLocate({
+        lat: location.coordinates.lat,
+        lng: location.coordinates.lng,
+      });
+    }
+  }, [location.coordinates]);
+
+  useEffect(() => {
+    if (location.loaded && locate.lat !== 0 && locate.lng !== 0) {
+      const fetchKakaoAddress = async () => {
+        try {
+          const res = await kakaoLocation(locate);
+          const kakaoAddress = res?.data?.documents[0]?.address_name;
+          setAddress(kakaoAddress || '주소를 불러오고 있어요!');
+        } catch (error: any) {
+          console.error(error);
+        }
+      };
+      fetchKakaoAddress();
+    }
+  }, [locate]);
+
   return (
     <>
-      <S.Container className="location">
+      <S.Container className="location" onClick={handleRefresh}>
         <S.LocationIconWrapper className="location__icon">
           <MapPin
             size={27}
@@ -19,7 +53,7 @@ const Location = () => {
           />
         </S.LocationIconWrapper>
         <S.LocationTextWrapper className="location__text">
-          광주 북구 용봉동
+          {location.loaded ? address : <span>주소를 불러오고 있어요!</span>}
         </S.LocationTextWrapper>
         <ArrowCounterClockwise
           size={17}
