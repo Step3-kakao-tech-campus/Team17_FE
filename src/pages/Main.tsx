dc6e8b3b91ea81d3ea4588ede9e555486641ac90
황수연 <202918@jnu.ac.kr>
2023-10-13 22:31:18 +0900

    feat: 사용자 위치 상태 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,13 +5,20 @@ import MainListTemplate from '../components/templates/MainListTemplate';
 import * as S from '../styles/layout/MainLayout';
 import MainGNB from '../components/organisms/MainGNB';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, Suspense } from 'react';
 import { AppDispatch, RootState } from '../store';
 import { setUser } from '../store/slices/userSlice';
 import { getLocalStorage } from '../utils/localStorage';
+import { fetchNotifications } from '../apis/notification';
+import { useQuery } from 'react-query';
+import MainListLoading from '../components/molecules/MainListLoading';
+import useGeolocation from '../hooks/useGeolocation';
 
 const Main = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [address, setAddress] = useState('');
+  const location = useGeolocation();
+  
   // 사용자가 메인 페이지에 접근했을 때 브라우저에서 사용자 로그인 상태 확인
   // const dispatch = useDispatch<AppDispatch>();
   // const user = useSelector((state: RootState) => state.user.user);
@@ -31,9 +38,11 @@ const Main = () => {
   return (
     <S.Container>
       <MainGNB setModalOpen={setModalOpen} />
-      <Location />
+      <Location location={location} address={address} setAddress={setAddress}/>
       <Carousel />
-      <MainListTemplate modalOpen={modalOpen} setModalOpen={setModalOpen} />
+      <Suspense fallback={<MainListLoading />}>
+      <MainListTemplate address={address} modalOpen={modalOpen} setModalOpen={setModalOpen}/>
+      </Suspense>
       <BottomNavBar />
     </S.Container>
   );
