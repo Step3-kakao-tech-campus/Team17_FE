f73c4d36ac7a94259d3d211a84bb4209a9b7a2fc
황수연 <202918@jnu.ac.kr>
2023-10-13 23:22:41 +0900

    feat: input 컴포넌트 onChange 함수 적용

--- src/components/atoms/input.tsx
+++ src/components/atoms/input.tsx
@@ -1,12 +1,14 @@
 type InputProps = {
   className?: string;
   placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const Input = ({ className, placeholder, ...props }: InputProps) => {
+const Input = ({ className, placeholder, value, onChange, ...props }: InputProps) => {
   return (
     <>
-      <input className={`myStyle ${className}`} placeholder={placeholder} {...props} />
+      <input className={`myStyle ${className}`} placeholder={placeholder} value={value} onChange={onChange}{...props}/>
     </>
   );
 };
