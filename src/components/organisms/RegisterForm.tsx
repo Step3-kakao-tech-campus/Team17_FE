037f08459ed78b0e08800e45a6ed1dff883d0e13
황수연 <202918@jnu.ac.kr>
2023-10-13 22:29:23 +0900

    fix: 유효성 검사 적용 버그 수정

--- src/components/organisms/RegisterForm.tsx
+++ src/components/organisms/RegisterForm.tsx
@@ -38,7 +38,7 @@ const RegisterForm = () => {
 
   const navigate = useNavigate();
 
-  const isValid = Object.values(invalidCheck).every((value) => value);
+  const isValid = Object.values(invalidCheck).every((value) => value === true);
 
   return (
     <>
