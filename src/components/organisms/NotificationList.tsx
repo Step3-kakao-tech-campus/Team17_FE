03a50e4e606157a56bdc22a10bdc1f4787717a81
황수연 <202918@jnu.ac.kr>
2023-10-15 16:42:42 +0900

    refactor: 변수 이름 변경

--- src/components/organisms/NotificationList.tsx
+++ src/components/organisms/NotificationList.tsx
@@ -5,24 +5,24 @@ import React from 'react';
 
 interface Notification {
   dog: {
-    name: string,
-    sex: string,
-    breed: string,
-    image: string,
-    age: number,
-  },
-  title: string,
-  dog_bowl: string,
-  id: number,
-  lng: number,
-  lat: number,
+    name: string;
+    sex: string;
+    breed: string;
+    image: string;
+    age: number;
+  };
+  title: string;
+  dog_bowl: string;
+  id: number;
+  lng: number;
+  lat: number;
 }
 
 type NotificationListProps = {
-  notification: Array<Notification>;
+  notifications?: Array<Notification>;
 };
 
-const NotificationList = ({notification}: NotificationListProps) => {
+const NotificationList = ({ notifications }: NotificationListProps) => {
   const handleAddNotification = () => {
     // 공고글 올리기 버튼 클릭 시 동작
     // 공고글 올리기 페이지로 이동
@@ -36,14 +36,16 @@ const NotificationList = ({notification}: NotificationListProps) => {
           <PlusCircle size={19} className="add__item" />
         </S.AddItemButton>
       </S.ButtonWrapper>
-      {notification.length  ? notification.map((item) => (
-        <ListItem
-          key={item.id}
-          dog={item.dog}
-          title={item.title}
-          dog_bowl={item.dog_bowl}
-        />
-      )) : (
+      {notifications ? (
+        notifications.map((notification) => (
+          <ListItem
+            key={notification.id}
+            dog={notification.dog}
+            title={notification.title}
+            dog_bowl={notification.dog_bowl}
+          />
+        ))
+      ) : (
         <div>공고글이 없습니다.</div>
       )}
     </S.Container>
