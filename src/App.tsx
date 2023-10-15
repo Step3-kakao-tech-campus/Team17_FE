253cb0ada13fcb4fb11289b0dd28c9769fc42637
황수연 <202918@jnu.ac.kr>
2023-10-16 00:35:04 +0900

    feat: 결제하기 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,24 @@ import Register from './pages/Register';
 import Login from './pages/Login';
 import OnBoarding from './pages/OnBoarding';
 import Main from './pages/Main';
-// import MainLayout from './components/layouts/MainLayout';
 import ProfilePage from './pages/ProfilePage';
 import ChatList from './pages/ChatList';
 import ChatRoom from './pages/ChatRoom';
+import Payment from './pages/Payment';
 
 const App = () => {
   return (
     <>
       <Router>
         <Routes>
-          {/* <Route element={<MainLayout />}> */}
           <Route path="/" element={<Main />} />
-          {/* </Route> */}
           <Route path="/signin" element={<Login />} />
           <Route path="/signup" element={<Register />} />
           <Route path="/onboard" element={<OnBoarding />} />
           <Route path="/profile" element={<ProfilePage />} />
           <Route path="/chatlist" element={<ChatList />} />
           <Route path="/chatroom" element={<ChatRoom />} />
+          <Route path="/payments" element={<Payment />} />
         </Routes>
       </Router>
     </>
