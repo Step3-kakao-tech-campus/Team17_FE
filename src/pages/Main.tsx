c21849d2f5a204abaadbe7920792615285fdcdb9
황수연 <202918@jnu.ac.kr>
2023-10-15 16:43:44 +0900

    feat: 검색 필터 상위 컴포넌트로 이동

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -13,13 +13,65 @@ import { fetchNotifications } from '../apis/notification';
 import { useQuery } from 'react-query';
 import MainListLoading from '../components/molecules/MainListLoading';
 import useGeolocation from '../hooks/useGeolocation';
+import { useDebounce } from '../hooks/useDebounce';
+import axios from 'axios';
+
+type Filter = {
+  size: string[];
+  breed: string[];
+};
 
 const Main = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [notificationList, setNotificationList] = useState<Array<Notification>>(
+    [],
+  );
   const [search, setSearch] = useState('');
   const [address, setAddress] = useState('');
   const location = useGeolocation();
-  
+  const [selectedFilter, setSelectedFilter] = useState<Filter>({
+    size: [],
+    breed: [],
+  });
+
+  // 사용자가 검색창을 입력하면 검색어를 서버로 전송하여 검색 결과를 받아온다.
+  const debouncedSearch = useDebounce(search, 500);
+  useEffect(() => {
+    if (debouncedSearch) {
+      fetchSearchNotifications(debouncedSearch);
+    }
+  }),
+    [debouncedSearch];
+
+  const fetchSearchNotifications = async (searchTerm: string) => {
+    const filterUrlTerm = selectedFilter.size
+      .map((size, idx) => {
+        if (idx === selectedFilter.size.length - 1) {
+          return `size=${size}`;
+        } else {
+          return `size=${size}&`;
+        }
+      })
+      .concat(
+        selectedFilter.breed.map((breed, idx) => {
+          if (idx === selectedFilter.breed.length - 1) {
+            return `breed=${breed}`;
+          } else {
+            return `breed=${breed}&`;
+          }
+        }),
+      );
+
+    try {
+      const response = await axios.get(
+        `api/home?${filterUrlTerm}&word=${searchTerm}`, // 예시 코드
+      );
+      setNotificationList(response.data.response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   // 사용자가 메인 페이지에 접근했을 때 브라우저에서 사용자 로그인 상태 확인
   // const dispatch = useDispatch<AppDispatch>();
   // const user = useSelector((state: RootState) => state.user.user);
@@ -38,11 +90,22 @@ const Main = () => {
 
   return (
     <S.Container>
-      <MainGNB setModalOpen={setModalOpen} search={search} setSearch={setSearch}/>
-      <Location location={location} address={address} setAddress={setAddress}/>
+      <MainGNB
+        setModalOpen={setModalOpen}
+        search={search}
+        setSearch={setSearch}
+      />
+      <Location location={location} address={address} setAddress={setAddress} />
       <Carousel />
       <Suspense fallback={<MainListLoading />}>
-      <MainListTemplate address={address} modalOpen={modalOpen} setModalOpen={setModalOpen} search={search}/>
+        <MainListTemplate
+          address={address}
+          modalOpen={modalOpen}
+          setModalOpen={setModalOpen}
+          search={search}
+          selectedFilter={selectedFilter}
+          setSelectedFilter={setSelectedFilter}
+        />
       </Suspense>
       <BottomNavBar />
     </S.Container>
