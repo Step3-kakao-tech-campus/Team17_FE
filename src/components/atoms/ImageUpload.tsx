00deccf1e5cef6fb35c96f9290d1adebbed411a0
YANGSEOKWOO <didtjrdn030@naver.com>
2023-10-15 19:56:49 +0900

    comment: post 요청추가

--- src/components/atoms/ImageUpload.tsx
+++ src/components/atoms/ImageUpload.tsx
@@ -28,4 +28,21 @@ const ImageUpload = () => {
       onClick={onUploadImageClick}
     ></input>
   );
-};
+}; // const plusDog = () => {
+//   postDog({
+//     // FIXME :: userid는 임의로 넣은 값, 수정필요
+//     userid: 1,
+//     image: dogProfile.image,
+//     name: dogProfile.name,
+//     sex: dogProfile.sex,
+//     breed: dogProfile.breed,
+//     specificity: dogProfile.specificity,
+//     age: dogProfile.age,
+//   })
+//     .then(() => {
+//       alert('반려견 등록이 완료되었습니다!');
+//     })
+//     .catch((err: { request: { response: string } }) => {
+//       console.log(err.request.response);
+//     });
+// };
