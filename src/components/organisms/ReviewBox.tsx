a1a41806aab1b8f3b750fb2a68b0f4f0267bbdd6
황수연 <202918@jnu.ac.kr>
2023-10-17 09:15:28 +0900

    feat: 리뷰 페이지 리뷰 작성란 구현

--- src/components/organisms/ReviewBox.tsx
+++ src/components/organisms/ReviewBox.tsx
@@ -1,10 +1,14 @@
 import { CheckCircle } from '@phosphor-icons/react';
 import * as S from '../../styles/organisms/ReviewBox';
+import { useState } from 'react';
+import Slider from 'rc-slider';
+import 'rc-slider/assets/index.css';
+import Image from '../atoms/Image';
 
 const dogOwner = [
   '온순해요',
   '공고대로에요',
-  '강아지 용품 준비가 잘 되어있어요',
+  '강아지 용품 준비가\n잘 되어있어요',
   '훈련되어 있어요',
 ];
 
@@ -17,6 +21,12 @@ const partTimeWorker = [
 
 const ReviewBox = () => {
   const user = 'dogOwner';
+  const [sliderValue, setSliderValue] = useState(39.5);
+
+  const handleSliderChange = (e: any) => {
+    setSliderValue(e);
+  };
+
   return (
     <S.Container>
       <div>
@@ -30,46 +40,95 @@ const ReviewBox = () => {
           />
           <span className="dog__name">복슬이</span>
         </S.ProfileWrapper>
-        <S.CheckboxWrapper>
-          <div>
-            <CheckCircle
-              color="#a59d52"
-              weight="fill"
-              size={20}
-              className="check__icon"
+        <div>
+          <S.CheckboxTable>
+            <S.CheckboxTr>
+              <td>
+                <S.CheckboxLabel>
+                  <CheckCircle
+                    color="#a59d52"
+                    weight="fill"
+                    size={20}
+                    className="check__icon"
+                    style={{ marginLeft: '1rem', paddingRight: '0.5rem' }}
+                  />
+                  {user === 'dogOwner' ? dogOwner[0] : partTimeWorker[0]}
+                </S.CheckboxLabel>
+              </td>
+              <td>
+                <S.CheckboxLabel>
+                  <CheckCircle
+                    color="#a59d52"
+                    weight="fill"
+                    size={20}
+                    className="check__icon"
+                    style={{ marginLeft: '1rem', paddingRight: '0.5rem' }}
+                  />
+                  {user === 'dogOwner' ? dogOwner[1] : partTimeWorker[1]}
+                </S.CheckboxLabel>
+              </td>
+            </S.CheckboxTr>
+            <S.CheckboxTr>
+              <td>
+                <S.CheckboxLabel>
+                  <CheckCircle
+                    color="#a59d52"
+                    weight="fill"
+                    size={20}
+                    className="check__icon"
+                    style={{ marginLeft: '1rem', paddingRight: '0.5rem' }}
+                  />
+                  {user === 'dogOwner' ? dogOwner[2] : partTimeWorker[2]}
+                </S.CheckboxLabel>
+              </td>
+              <td>
+                <S.CheckboxLabel>
+                  <CheckCircle
+                    color="#a59d52"
+                    weight="fill"
+                    size={20}
+                    className="check__icon"
+                    style={{ marginLeft: '1rem', paddingRight: '0.5rem' }}
+                  />
+                  {user === 'dogOwner' ? dogOwner[3] : partTimeWorker[3]}
+                </S.CheckboxLabel>
+              </td>
+            </S.CheckboxTr>
+          </S.CheckboxTable>
+        </div>
+        <S.ReviewWrapper>
+          <S.ReviewTitle>리뷰 작성</S.ReviewTitle>
+          <S.ReviewContent
+            placeholder="솔직한 후기를 작성해주세요!"
+            name="review"
+            id="review"
+          ></S.ReviewContent>
+        </S.ReviewWrapper>
+        <S.ReviewSliderWrapper>
+          <S.ReviewSliderTitle className="trust">
+            개밥그릇 - 신뢰지수{' '}
+          </S.ReviewSliderTitle>
+          <Slider
+            value={sliderValue}
+            className="slider"
+            onChange={handleSliderChange}
+            styles={{
+              track: { backgroundColor: '#a59d52' }, // 트랙 배경색
+              handle: { backgroundColor: '#a59d52', borderColor: '#a59d52' }, // 핸들 배경색
+            }}
+          />
+          <S.ReviewBowl>
+            {sliderValue}%
+            <Image
+              src="/images/dog-bowl.png"
+              alt="개밥그릇"
+              className="dog__bowl"
             />
-            {user === 'dogOwner' ? dogOwner[0] : partTimeWorker[0]}
-          </div>
-          <div>
-            <CheckCircle
-              color="#a59d52"
-              weight="fill"
-              size={20}
-              className="check__icon"
-            />
-            {user === 'dogOwner' ? dogOwner[1] : partTimeWorker[1]}
-          </div>
-        </S.CheckboxWrapper>
-        <S.CheckboxWrapper>
-          <div>
-            <CheckCircle
-              color="#a59d52"
-              weight="fill"
-              size={20}
-              className="check__icon"
-            />
-            {user === 'dogOwner' ? dogOwner[2] : partTimeWorker[2]}
-          </div>
-          <div>
-            <CheckCircle
-              color="#a59d52"
-              weight="fill"
-              size={20}
-              className="check__icon"
-            />
-            {user === 'dogOwner' ? dogOwner[3] : partTimeWorker[3]}
-          </div>
-        </S.CheckboxWrapper>
+          </S.ReviewBowl>
+        </S.ReviewSliderWrapper>
+        <S.ButtonWrapper>
+          <S.Button>리뷰 등록하기</S.Button>
+        </S.ButtonWrapper>
       </div>
     </S.Container>
   );
