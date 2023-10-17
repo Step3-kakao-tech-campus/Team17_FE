3ff03426c6b341d634373dd58cb569ffa43a8a44
황수연 <202918@jnu.ac.kr>
2023-10-17 09:18:24 +0900

    design: 리뷰페이지 박스 스타일 적용

--- src/styles/organisms/ReviewBox.ts
+++ src/styles/organisms/ReviewBox.ts
@@ -10,10 +10,17 @@ export const Container = styled.div`
 
 export const Title = styled.h2`
   margin: 0.5rem;
-  font-size: 1.2rem;
   padding: 0.5rem 0;
   color: black;
   border-bottom: 1px solid #dadada;
+
+  @media screen and (min-width: 768px) {
+    font-size: 1.2rem;
+  }
+
+  @media screen and (max-width: 768px) {
+    font-size: 1rem;
+  }
 `;
 
 export const Profile = styled.div`
@@ -41,10 +48,130 @@ export const ProfileImage = styled(Image)`
   margin: 0 1rem;
 `;
 
-export const CheckboxWrapper = styled.div`
+export const CheckboxTable = styled.table`
+  margin: 0.5rem auto;
+  margin-bottom: 1.2rem;
+  font-size: 0.9rem;
+  color: black;
+  white-space: pre;
+
+  @media screen and (min-width: 810px) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export const CheckboxTr = styled.tr`
+  line-height: 1.4rem;
+
+  & > td {
+    @media screen and (min-width: 768px) {
+      white-space: nowrap;
+    }
+  }
+`;
+
+export const CheckboxLabel = styled.span`
+  display: flex;
+  align-items: center;
+`;
+
+export const ReviewWrapper = styled.div`
+  margin: 0.5rem 0.7rem;
+  background-color: #f7f7f7;
+  height: 65%;
+`;
+
+export const ReviewTitle = styled.div`
+  margin: 0.5rem 0;
+  padding-top: 1rem;
+  padding-left: 1rem;
+  font-size: 1.1rem;
+  color: black;
+`;
+
+export const ReviewContent = styled.textarea`
+  border: none;
+  width: 80%;
+  height: 75%;
+  font-size: 0.9rem;
+  padding: 0.5rem 1rem;
+  overflow: auto;
+  scroll-behavior: smooth;
+  background-color: #f7f7f7;
+  resize: none;
+  color: black;
+  &::placeholder {
+    color: #7b7b7b;
+    font-family: 'IBMPlexSansKR-Regular';
+  }
+  font-family: 'IBMPlexSansKR-Regular';
+
+  &:focus {
+    outline: none;
+  }
+
+  &::-webkit-scrollbar {
+    visibility: hidden;
+  }
+`;
+
+export const ReviewSliderWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+
+  & > span {
+    font-size: 0.8rem;
+    white-space: nowrap;
+    padding-left: 0.5rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+export const ReviewBowl = styled.span`
+  .dog__bowl {
+    padding: 0 0.3rem;
+  }
+  color: #ff6060;
+`;
+
+export const ReviewSliderTitle = styled.span`
+  white-space: nowrap;
+  padding: 0 0.5rem;
+  color: black;
+`;
+
+export const ButtonWrapper = styled.div`
+  border: none;
+  color: white;
+  font-size: 1rem;
+  margin-top: 0.5rem;
+  cursor: pointer;
+  margin: 0.5rem 0.6rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Button = styled.button`
+  width: 90%;
+  background-color: #a59d52;
+  border-radius: 20px;
+  color: white;
   font-size: 0.9rem;
-  padding: 1rem 0;
+  cursor: pointer;
+  position: absolute;
+  bottom: 0;
+  margin-bottom: 1rem;
+
+  &:focus {
+    outline: none;
+    background-color: #eba059;
+  }
 `;
