18006c45d864723e224734d84b9e481d204a226e
황수연 <202918@jnu.ac.kr>
2023-10-17 11:43:50 +0900

    feat: 비로그인시 접근 막기

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,25 @@ import ChatRoom from './pages/ChatRoom';
 import Payment from './pages/Payment';
 import Review from './pages/Review';
 import Submit from './pages/Submit';
+import AuthRoute from './pages/AuthRoute';
 
 const App = () => {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Main />} />
           <Route path="/signin" element={<Login />} />
           <Route path="/signup" element={<Register />} />
           <Route path="/onboard" element={<OnBoarding />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/chatlist" element={<ChatList />} />
-          <Route path="/chatroom" element={<ChatRoom />} />
-          <Route path="/payments" element={<Payment />} />
-          <Route path="/review" element={<Review />} />
-          <Route path="/submit" element={<Submit />} />
+          <Route path="/" element={<Main />} />
+          <Route element={<AuthRoute />}>
+            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/chatlist" element={<ChatList />} />
+            <Route path="/chatroom" element={<ChatRoom />} />
+            <Route path="/payments" element={<Payment />} />
+            <Route path="/review" element={<Review />} />
+            <Route path="/submit" element={<Submit />} />
+          </Route>
         </Routes>
       </Router>
     </>
