9d057befd237c5b9ee41397273df4eb1eb5fdb9b
황수연 <202918@jnu.ac.kr>
2023-10-17 09:15:56 +0900

    design: 작은 화면에서 스타일 깨짐 수정

--- src/styles/atoms/ErrorMsg.ts
+++ src/styles/atoms/ErrorMsg.ts
@@ -4,7 +4,7 @@ export const Msg = styled.div`
   font-size: 0.5em;
   color: red;
   margin-bottom: ${(props) => (props.children ? '1em' : '0')};
-  @media only screen and (max-width: 700px) {
+  @media only screen and (max-width: 768px) {
     margin-left: 10vw;
     margin-right: 10vw;
   }
