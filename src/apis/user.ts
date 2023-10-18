69969749abc256548acefb352107b11712344331
황수연 <202918@jnu.ac.kr>
2023-10-18 14:16:06 +0900

    update: url 경로 및 params 지정

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -6,17 +6,17 @@ export const register = (data: {
   username: string;
 }) => {
   const { email, password, username } = data;
-  return instance.post('/join', {
-    email,
-    password,
-    username,
+  return instance.post('/member/signup', {
+    email: email,
+    password: password,
+    nickname: username,
   });
 };
 
 export const login = (data: { email: string; password: string }) => {
   const { email, password } = data;
   return instance.post('/login', {
-    email,
-    password,
+    email: email,
+    password: password,
   });
 };
