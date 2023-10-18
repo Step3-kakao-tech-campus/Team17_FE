712fb793ea8de4c728f69556d4c62f20dddeaeba
황수연 <202918@jnu.ac.kr>
2023-10-18 10:53:05 +0900

    chore: base_url 추가

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
+    BASE_URL: process.env.VITE_APP_BASE_URL,
     'process.env': {
       REACT_APP_KAKAO_REST_API_KEY:
         process.env.VITE_REACT_APP_KAKAO_REST_API_KEY,
