830e7be3e32405f6c01f59ab186b1144b66746ff
황수연 <202918@jnu.ac.kr>
2023-10-19 13:20:36 +0900

    comment: 주석 제거

--- src/pages/Review.tsx
+++ src/pages/Review.tsx
@@ -4,9 +4,6 @@ import * as S from '../styles/pages/DescriptiontBoxContainer';
 import ReviewBox from '../components/organisms/ReviewBox';
 
 const Review = () => {
-  // 결제 페이지 이동시 매칭 아이디 전달해줘야 함.
-  // const {data: payment, isLoading} = useQuery("payment", () => getPayment(1));
-
   return (
     <S.Container>
       <DescriptionBoxTitle title="리뷰 작성하기" />
