be0c4af765eb1a55dbdb97acb56d664bd9972d06
황수연 <202918@jnu.ac.kr>
2023-10-19 09:36:55 +0900

    refactor: 사용하지 않는 msw 서버 함수 제거

--- src/msw/paymentHandler.ts
+++ src/msw/paymentHandler.ts
@@ -1,12 +1,24 @@
-import {rest} from 'msw';
+import { rest } from 'msw';
 import payment from './json/payment.json';
+import paymentJson from './json/get-payment.json';
 
 const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 
-export const getPayment = rest.get(`${BASE_URL}/api/payment`, async (req, res, ctx) => {
-  await sleep(200);
-  return res(ctx.status(200), ctx.json(payment));
-});
+export const setPayment = rest.post(
+  `${BASE_URL}/api/payment`,
+  async (req, res, ctx) => {
+    await sleep(200);
+    return res(ctx.status(200), ctx.json(payment));
+  },
+);
+
+export const getPayment = rest.get(
+  `${BASE_URL}/api/payment`,
+  async (req, res, ctx) => {
+    await sleep(200);
+    return res(ctx.status(200), ctx.json(paymentJson));
+  },
+);
 
 async function sleep(ms: number) {
   return new Promise((resolve) => {
