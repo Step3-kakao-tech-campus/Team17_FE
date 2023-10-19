3a0ec2a854c6db2b34f18ac5e7696d0889b099a3
황수연 <202918@jnu.ac.kr>
2023-10-19 09:36:24 +0900

    fix: 로그인/회원가입 get에서 post로 요청 메소드 수정

--- src/msw/loginHandler.ts
+++ src/msw/loginHandler.ts
@@ -1,18 +1,24 @@
-import {rest} from 'msw';
+import { rest } from 'msw';
 import login from './json/login.json';
 import loginError from './json/login-error.json';
 
 const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 
-export const getLogin = rest.get(`${BASE_URL}/api/login`, async (req, res, ctx) => {
-  await sleep(200);
-  return res(ctx.status(200), ctx.json(login));
-});
+export const setLogin = rest.post(
+  `${BASE_URL}/api/login`,
+  async (req, res, ctx) => {
+    await sleep(200);
+    return res(ctx.json(login));
+  },
+);
 
-export const getLoginError = rest.get(`${BASE_URL}/api/login/error`, async (req, res, ctx) => {
-  await sleep(200);
-  return res(ctx.status(401), ctx.json(loginError));
-});
+export const getLoginError = rest.post(
+  `${BASE_URL}/api/login/error`,
+  async (req, res, ctx) => {
+    await sleep(200);
+    return res(ctx.status(401), ctx.json(loginError));
+  },
+);
 
 async function sleep(ms: number) {
   return new Promise((resolve) => {
--- src/msw/registerHandler.ts
+++ src/msw/registerHandler.ts
@@ -1,18 +1,24 @@
-import {rest} from 'msw';
-import register from './json/register.json';
+import { rest, RestContext } from 'msw';
+import registerJson from './json/register.json';
 import registerError from './json/register-error.json';
 
 const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 
-export const getRegister = rest.get(`${BASE_URL}/api/register`, async (req, res, ctx) => {
-  await sleep(200);
-  return res(ctx.status(200), ctx.json(register));
-});
+export const register = rest.post(
+  `${BASE_URL}/api/member/signup`,
+  async (req, res, ctx: RestContext) => {
+    await sleep(200);
+    return res(ctx.status(200), ctx.json(registerJson));
+  },
+);
 
-export const getRegisterError = rest.get(`${BASE_URL}/api/register/error`, async (req, res, ctx) => {
-  await sleep(200);
-  return res(ctx.status(401), ctx.json(registerError));
-});
+export const getRegisterError = rest.post(
+  `${BASE_URL}/api/member/signup/error`,
+  async (req, res, ctx: RestContext) => {
+    await sleep(200);
+    return res(ctx.status(401), ctx.json(registerError));
+  },
+);
 
 async function sleep(ms: number) {
   return new Promise((resolve) => {
