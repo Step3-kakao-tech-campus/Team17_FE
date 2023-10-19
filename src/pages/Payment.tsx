848dec05fcb0abff4d800f8a57a2b389102d0bc3
황수연 <202918@jnu.ac.kr>
2023-10-19 13:20:19 +0900

    feat: 결제 페이지 api 연결

--- src/pages/Payment.tsx
+++ src/pages/Payment.tsx
@@ -1,50 +1,24 @@
-import { useEffect, useState } from 'react';
+import { Suspense } from 'react';
 import DescriptionBox from '../components/atoms/DescriptionBox';
 import DescriptionBoxTitle from '../components/atoms/DescriptionBoxTitle';
 import PayBox from '../components/organisms/PayBox';
 import * as S from '../styles/pages/DescriptiontBoxContainer';
-import axios from 'axios';
 import { useQuery } from 'react-query';
 import { getPayment } from '../apis/payment';
-
-type paymentProps = {
-  userId: 1;
-  profile: '/images/dog-sample.png';
-  walkStart: '2023-07-18T05:56:34.157+00:00';
-  walkEnd: '2023-07-18T07:56:34.157+00:00';
-  notificationId: 2;
-  coin: 10000;
-};
+import { Spinner } from '@phosphor-icons/react';
 
 const Payment = () => {
-  const [payment, setPayment] = useState<paymentProps>();
   // 결제 페이지 이동시 매칭 아이디 전달해줘야 함.
-  // const {data: payment, isLoading} = useQuery("payment", () => getPayment(1));
-
-  useEffect(() => {
-    const mswData = async () => {
-      try {
-        const res = await axios({
-          method: 'get',
-          url: 'api/payment',
-          params: {
-            id: 1,
-          },
-        });
-        setPayment(res.data.response);
-        console.log('payment: ', res.data.response);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-
-    mswData();
-  }, []);
+  const { data: payment, isLoading } = useQuery('payment', () => getPayment(1));
 
   return (
     <S.Container>
       <DescriptionBoxTitle title="결제하기" />
-      <DescriptionBox>{payment && <PayBox payment={payment} />}</DescriptionBox>
+      <DescriptionBox>
+        <Suspense fallback={<Spinner />}>
+          {payment && <PayBox payment={payment.data.response} />}
+        </Suspense>
+      </DescriptionBox>
     </S.Container>
   );
 };
