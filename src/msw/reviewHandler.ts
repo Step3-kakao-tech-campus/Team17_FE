b6517c21c77c9985f6deea79aadc411d0e8a6ad5
황수연 <202918@jnu.ac.kr>
2023-10-19 13:19:33 +0900

    feat: 이름 변경

--- src/msw/reviewHandler.ts
+++ src/msw/reviewHandler.ts
@@ -1,12 +1,15 @@
-import {rest} from 'msw';
+import { rest } from 'msw';
 import review from './json/review.json';
 
 const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
 
-export const getReview = rest.get(`${BASE_URL}/api/review`, async (req, res, ctx) => {
-  await sleep(200);
-  return res(ctx.status(200), ctx.json(review));
-});
+export const setReview = rest.post(
+  `${BASE_URL}/api/review`,
+  async (req, res, ctx) => {
+    await sleep(200);
+    return res(ctx.status(200), ctx.json(review));
+  },
+);
 
 async function sleep(ms: number) {
   return new Promise((resolve) => {
