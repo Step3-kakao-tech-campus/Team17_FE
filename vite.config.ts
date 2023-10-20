2121e1c8b12f76ceed382c08c84de497bbaae74c
YANGSEOKWOO <didtjrdn030@naver.com>
2023-10-21 01:04:06 +0900

    feat: Kakao API연결

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
+    BASE_URL: process.env.VITE_APP_BASE_URL,
     'process.env': {
       REACT_APP_KAKAO_REST_API_KEY:
         process.env.VITE_REACT_APP_KAKAO_REST_API_KEY,
+      REACT_APP_KAKAO_JAVASCRIPT_KEY:
+        process.env.VITE_REACT_APP_KAKAO_JAVASCRIPT_KEY,
     },
   },
 });
