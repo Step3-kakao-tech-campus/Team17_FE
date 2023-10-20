044fb505231e8368c2fd461425702c79cf18f86d
황수연 <202918@jnu.ac.kr>
2023-10-20 20:45:56 +0900

    chore: pwa 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,39 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: '모르는개산책',
+        short_name: '모르는개산책',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#000000',
+        icons: [
+          {
+            src: '/images/logo-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/images/logo-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+    ,
+    react(),
+  ],
   define: {
     BASE_URL: process.env.VITE_APP_BASE_URL,
     'process.env': {
