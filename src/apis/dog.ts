4c87ec41ceb4a1cf67cb44aaa2b87db0aeaa52af
YANGSEOKWOO <didtjrdn030@naver.com>
2023-10-21 13:20:14 +0900

    comment: 강아지 api 주석추가

--- src/apis/dog.ts
+++ src/apis/dog.ts
@@ -1,6 +1,11 @@
 import axios from 'axios';
 import { instance } from './index';
 // 강아지 등록하기 => formData
+/**
+ * 강아지의 프로필을 수정하는 api
+ * @param data
+ * @returns formData
+ */
 export const postDogProfile = (data: FormData) => {
   return axios.post('/profile/dog/', data, {
     headers: {
@@ -8,7 +13,19 @@ export const postDogProfile = (data: FormData) => {
     },
   });
 };
-
+/**
+ * 상세 공고글에서, 본인이 가진 강아지를 가져오는 api
+ * @returns dogs
+ */
 export const getDog = () => {
   return instance.get('/notification');
+};
+
+/**
+ * 강아지의 상세 프로필을 가져오는 api
+ * @param dogId
+ * @returns Dog Profile
+ */
+export const getDogProfile = (dogId: number | null) => {
+  return instance.get(`/profile/dog${dogId}`);
 };
