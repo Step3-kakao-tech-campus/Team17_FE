faec334498505d9c1f41106f5a2b105cbc03d281
Chaeeun Lee <cyleecodms@naver.com>
2023-10-29 22:45:46 +0900

    style: 통일성을 위한 styled component 분리

--- src/components/template/MatchListTemplate.tsx
+++ src/components/template/MatchListTemplate.tsx
@@ -1,30 +1,12 @@
 import MatchList from '../organisms/MatchList';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  @media screen and (max-width: 768px) {
-    width: 90vw;
-    margin: 0 5vw;
-  }
-  @media screen and (min-width: 769px) {
-    width: 78vw;
-    margin: 0 10vw;
-  }
-`;
-
-const MatchRoom = styled.div`
-  text-align: center;
-  font-size: 32px;
-  margin-top: 80px;
-  margin-bottom: 80px;
-`;
+import * as S from '../../styles/templates/MatchListTemplate';
 
 const MatchListTemplate = () => {
   return (
-    <Container>
-      <MatchRoom>지원자 매칭</MatchRoom>
+    <S.Container>
+      <S.MatchRoom>지원자 매칭</S.MatchRoom>
       <MatchList />
-    </Container>
+    </S.Container>
   );
 };
 
