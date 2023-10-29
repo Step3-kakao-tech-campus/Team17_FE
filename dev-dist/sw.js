07e061d78947c438fd5293afc52b091b6407c72b
황수연 <202918@jnu.ac.kr>
2023-10-29 23:12:01 +0900

    fix: msw 의존성 충돌로 인해 이전 코드로 변경

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s21a8qiiqao"
+    "revision": "0.f5lqhpt8v6g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
