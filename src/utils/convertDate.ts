7d8f02947fa06ffe1e9c03ee879328ef4c05df9e
YANGSEOKWOO <didtjrdn030@naver.com>
2023-10-31 21:45:06 +0900

    feat: 날짜 변경 format 변경

--- src/utils/convertDate.ts
+++ src/utils/convertDate.ts
@@ -1,28 +1,19 @@
-type DateRangeProps = {
-  startDate: string;
-  endDate: string;
-};
-
-/**
- * ISO 8601형식의 start, end값을 받아 문자열 형태로 출력하는 함수
- * @param startDate : 산책시작시간
- * @param endDate : 산책종료시간
- * @returns 23.07.18 07:00 ~ 08:00의 형태로 반환
- */
 export const convertDate = ({ startDate, endDate }: DateRangeProps): string => {
-  // ISO 8601 형식 =>  Date 객체로 파싱
   const startDateTime = new Date(startDate);
   const endDateTime = new Date(endDate);
 
-  // 포맷
-  const formattedStartDate = `${startDateTime.getFullYear() % 100}.${(
+  const days = ['일', '월', '화', '수', '목', '금', '토'];
+  const startDay = days[startDateTime.getDay()];
+  const endDay = days[endDateTime.getDay()];
+
+  const formattedStartDate = `${startDateTime.getFullYear()}.${(
     startDateTime.getMonth() + 1
   )
     .toString()
     .padStart(2, '0')}.${startDateTime
     .getDate()
     .toString()
-    .padStart(2, '0')} ${startDateTime
+    .padStart(2, '0')} ${startDay} ${startDateTime
     .getHours()
     .toString()
     .padStart(2, '0')}:${startDateTime
@@ -35,6 +26,5 @@ export const convertDate = ({ startDate, endDate }: DateRangeProps): string => {
     .toString()
     .padStart(2, '0')}:${endDateTime.getMinutes().toString().padStart(2, '0')}`;
 
-  // 문자열로 반환
   return `${formattedStartDate} ~ ${formattedEndDate}`;
 };
