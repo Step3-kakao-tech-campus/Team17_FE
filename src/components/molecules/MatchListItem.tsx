02a64917ac490932ec1d040925d79ac7ed2c348d
Chaeeun Lee <cyleecodms@naver.com>
2023-11-01 00:28:56 +0900

    feat: 매칭목록 UI 수락, 거절버튼 추가

--- src/components/molecules/MatchListItem.tsx
+++ src/components/molecules/MatchListItem.tsx
@@ -15,23 +15,37 @@ type ListItemProps = {
 const MatchListItem = ({ apply }: ListItemProps) => {
   const { image, name, certification, experience } = apply;
 
-  const handleClick = () => {
-    console.log('click');
+  const handleApply = () => {
+    console.log('Apply clicked');
     // 해당 지원서 페이지로 이동
   };
 
+  const handleAccept = () => {
+    console.log('Accept clicked');
+    // Add logic for accepting the applicant
+  };
+
+  const handleReject = () => {
+    console.log('Reject clicked');
+    // Add logic for rejecting the applicant
+  };
+
   return (
-    <S.Container onClick={handleClick}>
+    <S.Container onClick={handleApply}>
       <S.ProfileImgWrapper>
-        <Image src={image} alt="지원자 임시 이미지" />
+        <Image src={image} alt="Applicant temporary image" />
       </S.ProfileImgWrapper>
-      <span>
-        <S.TextWrapper>
+      <S.TextWrapper>
+        <S.InfoWrapper>
           <S.ListTitle>닉네임 : {name}</S.ListTitle>
           <S.ListTitle>자격증 : {certification}</S.ListTitle>
           <S.ListTitle>경험 : {experience}</S.ListTitle>
-        </S.TextWrapper>
-      </span>
+        </S.InfoWrapper>
+      </S.TextWrapper>
+      <S.ButtonWrapper>
+        <S.AcceptButton onClick={handleAccept}>수락</S.AcceptButton>
+        <S.RejectButton onClick={handleReject}>거절</S.RejectButton>
+      </S.ButtonWrapper>
     </S.Container>
   );
 };
