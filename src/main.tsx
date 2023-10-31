00bb92bac0d5744c679dd8df13d5fdcc3a3a3e73
황수연 <202918@jnu.ac.kr>
2023-10-31 17:14:19 +0900

    update: error boundary 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App.tsx';
-import { ErrorBoundary } from 'react-error-boundary';
 import './index.css';
 import { worker } from './msw/browers.ts';
 import { BrowserRouter } from 'react-router-dom';
@@ -20,21 +19,14 @@ const queryClient = new QueryClient({
 //   worker.start();
 // }
 
-const ErrorFallback = ({ error }: { error: Error }) => (
-  <div>에러가 발생했습니다: {error.message}</div>
-);
+// const ErrorFallback = ({ error }: { error: Error }) => (
+//   <div>에러가 발생했습니다: {error.message}</div>
+// );
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
-      <ErrorBoundary
-        FallbackComponent={ErrorFallback}
-        onReset={() => {
-          // 에러가 발생했을 때 리셋 로직을 추가
-        }}
-      >
-        <App />
-      </ErrorBoundary>
+      <App />
     </QueryClientProvider>
   </BrowserRouter>,
 );
