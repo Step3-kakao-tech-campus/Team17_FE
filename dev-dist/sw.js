0dce66a2b5ba1cd58c4174854a32eb76708377b9
황수연 <202918@jnu.ac.kr>
2023-10-31 20:42:55 +0900

    chore: dist 파일 update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.67j648orefo"
+    "revision": "0.cuggakg3uu"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
