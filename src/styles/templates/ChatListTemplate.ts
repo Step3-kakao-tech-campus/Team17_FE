b7ece64eeee33078ac4a30762d90b534d10c9504
Chaeeun Lee <cyleecodms@naver.com>
2023-11-01 22:11:17 +0900

    design: 채팅목록, 매칭목록 페이지 리스트 출력 위치 수정

--- src/styles/templates/ChatListTemplate.ts
+++ src/styles/templates/ChatListTemplate.ts
@@ -14,6 +14,6 @@ export const Container = styled.div`
 export const ChatRoom = styled.div`
   text-align: center;
   font-size: 32px;
-  margin-top: 3rem;
-  margin-bottom: 5rem;
+  margin-top: 1rem;
+  margin-bottom: 3rem;
 `;
