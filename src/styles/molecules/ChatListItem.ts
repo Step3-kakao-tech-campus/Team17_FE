156b24c5deeda858f1614f9039738299a112e3fe
Chaeeun Lee <cyleecodms@naver.com>
2023-11-01 18:29:33 +0900

    채팅 목록 리스트 그림자 스타일 추가

--- src/styles/molecules/ChatListItem.ts
+++ src/styles/molecules/ChatListItem.ts
@@ -11,6 +11,7 @@ export const Container = styled.div`
   padding: 1rem 1rem;
   margin: 1rem 1rem;
   cursor: pointer;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); // 그림자
 `;
 
 export const ProfileImgWrapper = styled.span`
