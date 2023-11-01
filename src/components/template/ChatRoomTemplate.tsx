350f2987dcd9ebdeb4298b6ccf2c0e95e89b2e7c
Chaeeun Lee <cyleecodms@naver.com>
2023-11-02 00:33:53 +0900

    feat: 채팅방 페이지 템플릿 틀 수정

---
+++ src/components/template/ChatRoomTemplate.tsx
@@ -0,0 +1,12 @@
+import ChatList from '../organisms/ChatList';
+import * as S from '../../styles/templates/ChatListTemplate';
+
+const ChatListTemplate = () => {
+  return (
+    <S.Container>
+      <S.ChatRoom>채팅방</S.ChatRoom>
+    </S.Container>
+  );
+};
+
+export default ChatListTemplate;
