8b773776efc65d192bec2ef4aaa37a89ce1cc5a2
황수연 <202918@jnu.ac.kr>
2023-11-01 21:52:35 +0900

    feat: 알바생 위치 전송 및 산책 시작 종료 api 연동

--- src/components/organisms/CurrentWalkingMap.tsx
+++ src/components/organisms/CurrentWalkingMap.tsx
@@ -4,15 +4,14 @@ import * as S from '../../styles/organisms/CurrentWalkingMap';
 import { useMutation } from 'react-query';
 import { walkingStart, walkingEnd } from '../../apis/walking';
 import { useNavigate } from 'react-router-dom';
-import { useEffect } from 'react';
 
 const CurrentWalkingMap = () => {
   // 웹 워커 생성
   // const worker = new Worker('locationWorker.js');
   const navigate = useNavigate();
   const matchingId = 1;
-  const user = 'dogOwner';
-  const walkStatus = 'ACTIVATE';
+  const user: string = 'dogOwner';
+  const walkStatus: string = 'ACTIVATE';
   const buttonInnerText =
     walkStatus === 'ACTIVATE' ? '산책 종료하기' : '산책 시작하기';
 
@@ -28,72 +27,73 @@ const CurrentWalkingMap = () => {
     // TODO: BackCursor 클릭시 채팅방 페이지로 이동 기능 추가
   };
 
-  // const startLocationUpdate = () => {
-  //   if (navigator.geolocation) {
-  //     navigator.geolocation.getCurrentPosition(
-  //       (position) => {
-  //         const { latitude, longitude } = position.coords;
-  //         // 웹 워커에 위치 데이터 전송
-  //         sendToLocationToWorker(matchingId, latitude, longitude);
-  //       },
-  //       (err) => {
-  //         console.error('Error getting user location:', err);
-  //       },
-  //     );
-  //   } else {
-  //     console.error('Geolocation is not supported');
-  //   }
-  // };
+  const startLocationUpdate = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          // 웹 워커에 위치 데이터 전송
+          sendToLocationToWorker(matchingId, latitude, longitude);
+        },
+        (err) => {
+          console.error('Error getting user location:', err);
+        },
+      );
+    } else {
+      console.error('Geolocation is not supported');
+    }
+  };
 
   // 산책 종료 버튼 클릭 시 clearInterval을 통해 업데이트 중지
   const stopLocationUpdate = (intervalId: any) => {
     clearInterval(intervalId);
   };
 
-  // const sendToLocationToWorker = (
-  //   matchingId: number,
-  //   lat: number,
-  //   lng: number,
-  // ) => {
-  //   worker.postMessage({
-  //     matchingId,
-  //     lat,
-  //     lng,
-  //   });
-  // };
+  const sendToLocationToWorker = (
+    matchingId: number,
+    lat: number,
+    lng: number,
+  ) => {
+    worker.postMessage({
+      matchingId,
+      lat,
+      lng,
+    });
+  };
 
   const handleClickButton = () => {
-    // let intervalId: any;
-    // if (walkStatus === 'ACTIVATE') {
-    //   mutateWalkingEnd(matchingId, {
-    //     onSuccess: (res) => {
-    //       // 산책 종료 알림 보내기
-    //       alert('산책을 종료합니다!');
-    //       stopLocationUpdate(intervalId);
-    //       navigate('/review', {
-    //         state: {
-    //           memberId: res.data.response.memberId,
-    //           receiveMemberId: res.data.response.receiveMemberId,
-    //         },
-    //       });
-    //     },
-    //     onError: (error: any) => {
-    //       alert(error.response.message);
-    //     },
-    //   });
-    // } else {
-    //   mutateWalkingStart(matchingId, {
-    //     onSuccess: () => {
-    //       // 산책 시작 알림 보내기
-    //       alert('산책을 시작합니다!');
-    //       // 알바생이 산책 시작 버튼을 클릭하면 알바생 위치를 웹 워커를 통해 실시간 업데이트
-    //       intervalId = setInterval(startLocationUpdate, 5000); // 5초마다 업데이트
-    //     },
-    //     onError: (error: any) => {
-    //       alert(error.response.message);
-    //     },
-    //   });
-    // }
+    let intervalId: any;
+    if (walkStatus === 'ACTIVATE') {
+      mutateWalkingEnd(matchingId, {
+        onSuccess: (res) => {
+          // 산책 종료 알림 보내기
+          alert('산책을 종료합니다!');
+          stopLocationUpdate(intervalId);
+          navigate('/review', {
+            state: {
+              memberId: res.data.response.memberId,
+              receiveMemberId: res.data.response.receiveMemberId,
+            },
+          });
+        },
+        onError: (error: any) => {
+          alert(error.response.message);
+        },
+      });
+    } else {
+      mutateWalkingStart(matchingId, {
+        onSuccess: (res) => {
+          console.log('res', res);
+          // 산책 시작 알림 보내기
+          alert('산책을 시작합니다!');
+          // 알바생이 산책 시작 버튼을 클릭하면 알바생 위치를 웹 워커를 통해 실시간 업데이트
+          intervalId = setInterval(startLocationUpdate, 2000); // 2초마다 업데이트
+        },
+        onError: (error: any) => {
+          alert(error.response.message);
+        },
+      });
+    }
   };
 
   return (
