ad8d2d94eca0c89417940753a606793d19eecbde
Chaeeun Lee <cyleecodms@naver.com>
2023-11-02 00:35:29 +0900

    design: 채팅방 상단바 스타일 지정

---
+++ src/styles/molecules/ChatRoomBannerItem.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+export const Container = styled.div`
+  display: flex;
+  padding: 1rem 0 1rem 1rem;
+  justify-content: space-between;
+`;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  cursor: pointer;
+  width: 70%;
+  @media screen and (min-width: 860px) {
+    width: 85%;
+  }
+`;
+
+export const NameWrapper = styled.div`
+  display: flex;
+  justify-content: start;
+  align-items: center;
+  width: 70%;
+  @media screen and (min-width: 860px) {
+    width: 85%;
+  }
+
+  & > h1 {
+    margin-left: 1rem;
+  }
+`;
+
+export const LogoutButton = styled.button`
+  background-color: transparent;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:active {
+    outline: none;
+  }
+`;
