6cc96682a71a7ef5ea01f411df84368e5cc44f0c
황수연 <202918@jnu.ac.kr>
2023-11-01 17:56:46 +0900

    design: 큰 화면에서의 버튼 위치 하단으로 변경

--- src/components/organisms/PayBox.tsx
+++ src/components/organisms/PayBox.tsx
@@ -92,83 +92,85 @@ const PayBox = ({ payment }: paymentProps) => {
 
   return (
     <S.Container>
-      <div>
-        <S.Title>결제 정보 확인</S.Title>
-        {payment ? (
-          <>
-            <S.Profile>
-              <S.ProfileWrapper>
-                <S.ProfileImage
-                  src={'/images/dog-sample.png' || profile}
-                  alt="결제하기 견주 프로필"
-                  size="4"
-                  className="pay__profile"
-                />
-                <span className="dog__name">복슬이</span>
-              </S.ProfileWrapper>
-              <S.CoinWrapper>
-                <S.CoinWrapper>
-                  <span>멍코인</span>
-                  <PawPrint
-                    color="#a59d52"
-                    weight="fill"
-                    size={18}
-                    className="paw__icon"
+      <S.BottomContentWrapper>
+        <div>
+          <S.Title>결제 정보 확인</S.Title>
+          {payment ? (
+            <>
+              <S.Profile>
+                <S.ProfileWrapper>
+                  <S.ProfileImage
+                    src={'/images/dog-sample.png' || profile}
+                    alt="결제하기 견주 프로필"
+                    size="4"
+                    className="pay__profile"
                   />
+                  <span className="dog__name">복슬이</span>
+                </S.ProfileWrapper>
+                <S.CoinWrapper>
+                  <S.CoinWrapper>
+                    <span>멍코인</span>
+                    <PawPrint
+                      color="#a59d52"
+                      weight="fill"
+                      size={18}
+                      className="paw__icon"
+                    />
+                  </S.CoinWrapper>
+                  <span className="dog__coin">{userCoin}멍</span>
                 </S.CoinWrapper>
-                <span className="dog__coin">{userCoin}멍</span>
-              </S.CoinWrapper>
-            </S.Profile>
-            <S.WalkingWrapper>
-              <S.WalkingInfo>
-                <span className="dog__walking">산책 시작일</span>
-                <span>
-                  {startDate[0]} {startDate[1].substring(0, 5)}
-                </span>
-              </S.WalkingInfo>
-              <S.WalkingInfo>
-                <span className="dog__walking">산책 종료일</span>
-                <span>
-                  {endDate[0]} {endDate[1].substring(0, 5)}
-                </span>
-              </S.WalkingInfo>
-            </S.WalkingWrapper>
-            <S.PayServiceWrapper>
-              <S.PayServiceInfo>
-                <span className="dog__walking">서비스 금액</span>
-                <span>{coin} 멍</span>
-              </S.PayServiceInfo>
-              <S.PayServiceInfo>
-                <span className="dog__walking">서비스 수수료</span>
-                <span>+{comma(serviceCost)} 멍</span>
-              </S.PayServiceInfo>
-              <S.PayServiceInfo>
-                <span className="dog__walking">최종금액</span>
-                <span>
-                  <span className="total__price">{totalCost}</span> 멍
-                </span>
-              </S.PayServiceInfo>
-              <S.PayServiceInfo>
-                <span className="dog__walking">남은 멍코인</span>
-                <span>0 멍</span>
-              </S.PayServiceInfo>
-            </S.PayServiceWrapper>
-          </>
-        ) : (
-          <Spinner />
-        )}
-      </div>
-      <div>
-        <S.ServicePolicy onClick={handleAgree}>
-          {agree ? checkCircleIconFill() : checkCircleIcon()}
-          <span>서비스 이용약관 동의</span>
-        </S.ServicePolicy>
-        <S.ButtonWrapper>
-          <S.PayButton onClick={handlePayment} disabled={!payment}>
-            결제하기
-          </S.PayButton>
-        </S.ButtonWrapper>
-      </div>
+              </S.Profile>
+              <S.WalkingWrapper>
+                <S.WalkingInfo>
+                  <span className="dog__walking">산책 시작일</span>
+                  <span>
+                    {startDate[0]} {startDate[1].substring(0, 5)}
+                  </span>
+                </S.WalkingInfo>
+                <S.WalkingInfo>
+                  <span className="dog__walking">산책 종료일</span>
+                  <span>
+                    {endDate[0]} {endDate[1].substring(0, 5)}
+                  </span>
+                </S.WalkingInfo>
+              </S.WalkingWrapper>
+              <S.PayServiceWrapper>
+                <S.PayServiceInfo>
+                  <span className="dog__walking">서비스 금액</span>
+                  <span>{coin} 멍</span>
+                </S.PayServiceInfo>
+                <S.PayServiceInfo>
+                  <span className="dog__walking">서비스 수수료</span>
+                  <span>+{comma(serviceCost)} 멍</span>
+                </S.PayServiceInfo>
+                <S.PayServiceInfo>
+                  <span className="dog__walking">최종금액</span>
+                  <span>
+                    <span className="total__price">{totalCost}</span> 멍
+                  </span>
+                </S.PayServiceInfo>
+                <S.PayServiceInfo>
+                  <span className="dog__walking">남은 멍코인</span>
+                  <span>0 멍</span>
+                </S.PayServiceInfo>
+              </S.PayServiceWrapper>
+            </>
+          ) : (
+            <Spinner />
+          )}
+        </div>
+        <div>
+          <S.ServicePolicy onClick={handleAgree}>
+            {agree ? checkCircleIconFill() : checkCircleIcon()}
+            <span>서비스 이용약관 동의</span>
+          </S.ServicePolicy>
+          <S.ButtonWrapper>
+            <S.PayButton onClick={handlePayment} disabled={!payment}>
+              결제하기
+            </S.PayButton>
+          </S.ButtonWrapper>
+        </div>
+      </S.BottomContentWrapper>
     </S.Container>
   );
 };
