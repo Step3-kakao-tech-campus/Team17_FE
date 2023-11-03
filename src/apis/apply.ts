c269fb38806deb293d75d799faf7c9ba5fa32e2b
Chaeeun Lee <cyleecodms@naver.com>
2023-11-03 18:05:55 +0900

    feat: 지원서 조회 API 수정

--- src/apis/apply.ts
+++ src/apis/apply.ts
@@ -18,8 +18,7 @@ export const PostApply = (
 };
 
 export const GetApply = (memberId: number, applicationId: number) => {
-  return instance.get(`api/application?id=${memberId}`);
-  // return instance.get(`api/application/${memberId}`);
+  return instance.get(`api/application/${memberId}`);
 };
 
 export const GetMatch = (notificationId: number) => {
