db289c773db23c913c2935c52605f61ea3ebe6ff
YANGSEOKWOO <didtjrdn030@naver.com>
2023-11-04 17:57:25 +0900

    util: 날짜변환 기능 추가

--- src/utils/convertDate.ts
+++ src/utils/convertDate.ts
@@ -39,3 +39,16 @@ export const convertDate = ({ startDate, endDate }: DateRangeProps): string => {
 
   return `${formattedStartDate} ~ ${formattedEndDate}`;
 };
+
+export const convertDay = (isoDate: string): string => {
+  const date = new Date(isoDate);
+  const year = date.getFullYear().toString().substring(2);
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
+  const dayOfWeek = daysOfWeek[date.getDay()];
+
+  return `${year}.${month}.${day} ${dayOfWeek} ${hours}:${minutes}`;
+};
