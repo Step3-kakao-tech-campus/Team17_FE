2d9f13b3e70e8a9f5bc3ae0363d99eb054204268
황수연 <202918@jnu.ac.kr>
2023-11-05 03:11:33 +0900

    chore: 빌드 파일 업데이트

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.021oap9d2m"
+    "revision": "0.57gkahikbv"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
