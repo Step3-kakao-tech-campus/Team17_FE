0f495cf3164190a5e396b3a8dac0ebe7a03fd485
YANGSEOKWOO <didtjrdn030@naver.com>
2023-11-05 22:11:33 +0900

    fix:세부공고페이지 PageLoading 에러 해결

--- src/pages/DetailNotificationPage.tsx
+++ src/pages/DetailNotificationPage.tsx
@@ -1,33 +1,33 @@
 import { useParams } from 'react-router-dom';
 import DetailNotificationTemplate from '../components/templates/DetailNotificationTemplate';
-import { useQuery } from 'react-query';
 import { getNotificationById } from '../apis/notification';
 import PageLoading from '../components/atoms/PageLoading';
+import { useEffect, useState } from 'react';
 
 function DetailNotificationPage() {
   const { id } = useParams();
   if (!id) {
-    return <div> 유효하지 않은 id</div>;
+    return <div>유효하지 않은 id</div>;
   }
   const notificationId = parseInt(id, 10);
-  const { data, error, isLoading } = useQuery(
-    [`/notification/${notificationId}`, notificationId],
-    () => getNotificationById(notificationId),
-  );
-  if (isLoading) {
-    return <div> 로딩중 ...</div>;
-  }
-  if (data) {
-    console.log('data : ', data);
-  }
+  // const { data, error, isLoading } = useQuery(
+  //   [`/notification/${notificationId}`, notificationId],
+  //   () => getNotificationById(notificationId),
+  // );
+  const [data, setData] = useState();
+  useEffect(() => {
+    getNotificationById(notificationId)
+      .then((res) => setData(res.data.response))
+      .catch((err) => {
+        console.log('err', err);
+      });
+  });
+
   return (
-    <>
-      {!isLoading && data ? (
-        <DetailNotificationTemplate data={data?.data.response} />
-      ) : (
-        <PageLoading />
-      )}
-    </>
+    // {data ? (
+    //   <DetailNotificationTemplate data={data} />
+    // ):<PageLoading/>};
+    <>{data ? <DetailNotificationTemplate data={data} /> : <PageLoading />}</>
   );
 }
 
