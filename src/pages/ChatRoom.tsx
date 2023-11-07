b0aa72f2a027a376e8036915f5240d9df6fe7325
Chaeeun Lee <cyleecodms@naver.com>
2023-11-07 16:36:29 +0900

    feat: 채팅방 socket 통신 구현

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -1,11 +1,13 @@
-// import React from 'react'
-import ChatListTemplate from '../components/template/ChatRoomTemplate';
+import ChatRoomTemplate2 from '../components/template/ChatRoomTemplate2';
 import Container from '../components/atoms/Container';
 
 const ChatRoom = () => {
+  const roomId = 1; // 예시로 roomId를 설정
+  const memberId = 2; // 예시로 memberId를 설정
+
   return (
     <Container>
-      <ChatListTemplate />
+      <ChatRoomTemplate2 roomId={roomId} memberId={memberId} />
     </Container>
   );
 };
