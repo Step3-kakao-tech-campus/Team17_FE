2b9154f44efcc97d2b82878abb6e55f1f3d02499
YANGSEOKWOO <didtjrdn030@naver.com>
2023-11-08 17:17:42 +0900

    feat: NotReivew 컴포넌트 구현

--- src/components/atoms/NotReview.tsx
+++ src/components/atoms/NotReview.tsx
@@ -1,7 +1,24 @@
 import * as S from '../../styles/atoms/NotReview';
+import Image from './Image';
 
 function NotReview() {
-  return <S.Container> 확인 </S.Container>;
+  const img = '이미지';
+  const title = '귀여운 복슬이 산책시키실분~';
+  const time = '23.12.25~ 15:00~16:00';
+  return (
+    <S.Container>
+      {' '}
+      <Image src={img} alt="대체이미지" />
+      <S.ContentContainer>
+        <span className="title">{title}</span>
+        <div className="time">
+          <span className="walk"> 산책일자 </span>
+          <span> {time}</span>
+        </div>
+      </S.ContentContainer>
+      <S.Button> 작성 </S.Button>
+    </S.Container>
+  );
 }
 
 export default NotReview;
