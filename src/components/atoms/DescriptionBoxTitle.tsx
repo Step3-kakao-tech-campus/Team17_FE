ba6d339e14b13d58877144e8d7d512156f4cb32a
황수연 <202918@jnu.ac.kr>
2023-11-09 17:08:57 +0900

    feat: 뒤로가기 버튼 추가

--- src/components/atoms/DescriptionBoxTitle.tsx
+++ src/components/atoms/DescriptionBoxTitle.tsx
@@ -1,11 +1,23 @@
+import { CaretLeft } from '@phosphor-icons/react';
 import * as S from '../../styles/atoms/DescriptionBoxTitle';
+import { useNavigate } from 'react-router-dom';
 
 type DescriptionBoxTitleProps = {
   title: string;
 };
 
 const DescriptionBoxTitle = ({ title }: DescriptionBoxTitleProps) => {
-  return <S.PayTitle>{title}</S.PayTitle>;
+  const navigate = useNavigate();
+  return (
+    <S.PayTitle>
+      <CaretLeft
+        className="back__icon"
+        size={32}
+        onClick={() => navigate(-1)}
+      />
+      {title}
+    </S.PayTitle>
+  );
 };
 
 export default DescriptionBoxTitle;
