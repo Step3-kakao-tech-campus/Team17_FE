f75f7a650224580c813aaef5efa77ac30534d7f9
Chaeeun Lee <cyleecodms@naver.com>
2023-11-10 07:02:53 +0900

    feat: 채팅방 RoomId state값으로 불러오기 구현

--- src/pages/ChatRoom.tsx
+++ src/pages/ChatRoom.tsx
@@ -6,16 +6,21 @@ import { useLocation } from 'react-router-dom';
 const ChatRoom = () => {
   const { state } = useLocation();
 
-  const roomId = 1; // 예시로 roomId를 설정
-  const memberId = 2; // 예시로 memberId를 설정
-  const chatContent = 'test 채팅 내용';
+  // const messageType = "CHAT"
+  // const roomId = 1; // 예시로 roomId를 설정
+  const memberId = 1; // 예시로 memberId를 설정
+  // const chatContent = 'test 채팅 내용';
+  console.log('chatroom page로 가져온 state값 조회', state);
+  console.log('state값 중 chatRoomId 추출', state.userinfo.chatRoomId);
+  console.log('state값 중 memberId 추출', state.userinfo.memberId);
   return (
     <Container>
-      <ChatRoomBanner userInfo={state} />
+      <ChatRoomBanner />
       <ChatRoomTemplate2
-        roomId={roomId}
+        room_Id={state.userinfo.chatRoomId}
         memberId={memberId}
-        chatContent={chatContent}
+        // memberId={state.userinfo.memberId}
+        // chatContent={chatContent}
       />
     </Container>
   );
