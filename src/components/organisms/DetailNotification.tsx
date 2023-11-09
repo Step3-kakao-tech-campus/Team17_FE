6c9230ec9f6d56694fc8994db758224163dee47a
황수연 <202918@jnu.ac.kr>
2023-11-09 17:14:16 +0900

    update: 뒤로가기 경로 제거

--- src/components/organisms/DetailNotification.tsx
+++ src/components/organisms/DetailNotification.tsx
@@ -1,10 +1,5 @@
 import * as S from '../../styles/organisms/DetialNotification';
-import {
-  MapPin,
-  CaretCircleRight,
-  Plus,
-  CaretLeft,
-} from '@phosphor-icons/react';
+import { MapPin, CaretCircleRight } from '@phosphor-icons/react';
 import DogProfile from './DogProfile';
 import kakaoLocation from '../../utils/kakaoLocation';
 import { useEffect, useState } from 'react';
@@ -70,83 +65,85 @@ function DetailNotification({ data }: NotificationProps) {
     navigate(`/notification/${notiData.notificationId}/match`);
   };
   return (
-    <S.TopContainer>
-      <S.NotiTitle>
-        <S.Title>
-          <div className="arrow">
-            <BackBar to="/" />
-          </div>
-          <div className="subtitle">{notiData.title}</div>
-          <div className="blank">&nbsp;</div>
-        </S.Title>
-      </S.NotiTitle>
+    <>
+      <S.TopContainer>
+        <S.NotiTitle>
+          <S.Title>
+            <div className="arrow">
+              <BackBar />
+            </div>
+            <div className="subtitle">{notiData?.title}</div>
+            <div className="blank">&nbsp;</div>
+          </S.Title>
+        </S.NotiTitle>
 
-      <DescriptionBox>
-        <S.MainContainer>
-          {/* {notiData.isMine ? <div className=''> 공고글 수정하기 </div> : ''} */}
-          <DogProfile
-            img={notiData.dog.image}
-            name={notiData.dog.name}
-            breed={notiData.dog.breed}
-            age={notiData.dog.age}
-            size={notiData.dog.size}
-            userId={notiData.userId}
-          />
-          {/* 시간위치 컴포넌트 */}
-          <S.TimeLocationContainer>
-            <S.LocationContainer>
-              <MapPin fill="red" weight="fill" size={24} />
-              <span className="title"> 산책 위치</span>
-              <span className="map">{address}</span>
-            </S.LocationContainer>
-            <S.TimeContainer>
-              <div className="title"> 희망 시간 </div>
-              <div className="time">
-                <CaretCircleRight
-                  weight="fill"
-                  color="#D6CFA5"
-                  className="time__icon"
-                />
-                <span className="walking__time">
-                  {convertDate({
-                    startDate: notiData.start,
-                    endDate: notiData.end,
-                  })}
-                </span>
-                {/* <Plus onClick={onClickDateModal} size={16} /> */}
+        <DescriptionBox>
+          <S.MainContainer>
+            {/* {notiData.isMine ? <div className=''> 공고글 수정하기 </div> : ''} */}
+            <DogProfile
+              img={notiData.dog.image}
+              name={notiData.dog.name}
+              breed={notiData.dog.breed}
+              age={notiData.dog.age}
+              size={notiData.dog.size}
+              userId={notiData.userId}
+            />
+            {/* 시간위치 컴포넌트 */}
+            <S.TimeLocationContainer>
+              <S.LocationContainer>
+                <MapPin fill="red" weight="fill" size={24} />
+                <span className="title"> 산책 위치</span>
+                <span className="map">{address}</span>
+              </S.LocationContainer>
+              <S.TimeContainer>
+                <div className="title"> 희망 시간 </div>
+                <div className="time">
+                  <CaretCircleRight
+                    weight="fill"
+                    color="#D6CFA5"
+                    className="time__icon"
+                  />
+                  <span className="walking__time">
+                    {convertDate({
+                      startDate: notiData.start,
+                      endDate: notiData.end,
+                    })}
+                  </span>
+                  {/* <Plus onClick={onClickDateModal} size={16} /> */}
+                </div>
+              </S.TimeContainer>
+            </S.TimeLocationContainer>
+            <S.Container>
+              <div className="specificity">
+                <div className="title"> 특이사항 </div>
+                <S.Content
+                  placeholder="특이사항을 작성해주세요!"
+                  name="specificity"
+                  id="specificity"
+                  className="post"
+                  value={notiData.significant}
+                ></S.Content>
               </div>
-            </S.TimeContainer>
-          </S.TimeLocationContainer>
-          <S.Container>
-            <div className="specificity">
-              <div className="title"> 특이사항 </div>
-              <S.Content
-                placeholder="특이사항을 작성해주세요!"
-                name="specificity"
-                id="specificity"
-                className="post"
-                value={notiData.significant}
-              ></S.Content>
-            </div>
-            <div className="amount">
-              <span className="title"> 지불금액</span>
-              <div className="price">
-                <span className="coin">{comma(notiData.coin)}</span>
-                <span>멍</span>
+              <div className="amount">
+                <span className="title"> 지불금액</span>
+                <div className="price">
+                  <span className="coin">{comma(notiData.coin)}</span>
+                  <span>멍</span>
+                </div>
               </div>
-            </div>
-          </S.Container>
-          <S.ButtonContainer>
-            {/* TODO:: 지원자보기, 지원하기에 대한 라우팅 필요 */}
-            {notiData.isMine ? (
-              <S.Button onClick={onViewApplyClick}>지원자 보기</S.Button>
-            ) : (
-              <S.Button onClick={onApplyClick}>지원하기</S.Button>
-            )}
-          </S.ButtonContainer>
-        </S.MainContainer>
-      </DescriptionBox>
-    </S.TopContainer>
+            </S.Container>
+            <S.ButtonContainer>
+              {/* TODO:: 지원자보기, 지원하기에 대한 라우팅 필요 */}
+              {notiData.isMine ? (
+                <S.Button onClick={onViewApplyClick}>지원자 보기</S.Button>
+              ) : (
+                <S.Button onClick={onApplyClick}>지원하기</S.Button>
+              )}
+            </S.ButtonContainer>
+          </S.MainContainer>
+        </DescriptionBox>
+      </S.TopContainer>
+    </>
   );
 }
 
