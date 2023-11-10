27d4e85cf476edf33010152c23dc3d039a74bafa
Chaeeun Lee <cyleecodms@naver.com>
2023-11-11 06:12:33 +0900

    remove: 필요 없는 파일 삭제

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.b58h46bg0lo"
+    "revision": "0.d6k2bqrq7og"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
