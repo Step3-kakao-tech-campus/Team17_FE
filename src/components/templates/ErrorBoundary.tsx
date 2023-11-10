e61f7b7788a6f0afa579f43052162f93f4780af1
황수연 <202918@jnu.ac.kr>
2023-11-11 06:10:46 +0900

    fix: 렌더링 오류 해결

---
+++ src/components/templates/ErrorBoundary.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+import { ReactNode } from 'react';
+
+type ErrorBoundaryProps = {
+  children: ReactNode;
+};
+
+const ErrorBoundary = ({ children }: ErrorBoundaryProps) => {
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    const handleError = (error: ErrorEvent) => {
+      console.error('Error caught by error boundary:', error);
+      setHasError(true);
+    };
+
+    // 에러 이벤트 리스너 등록
+    window.addEventListener('error', handleError);
+
+    // 컴포넌트 언마운트 시 에러 이벤트 리스너 해제
+    return () => {
+      window.removeEventListener('error', handleError);
+    };
+  }, []);
+
+  if (hasError) {
+    window.location.reload();
+  }
+  return children;
+};
+
+export default ErrorBoundary;
